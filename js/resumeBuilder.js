629f02aa5ab4d9ff9041550be69ab78817fcf3be
David Kennedy <dkennedychicago@gmail.com>
2016-04-01 17:40:49 -0500

    Fixed Education and Map
    Added more jobs and Education, still working on getting all the jobs to
    show up.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -45,7 +45,7 @@ var bio = {
 		"email": "dkennedychicago@gmail.com",
 		"github": "dkennedychicago",
 		"twitter": "@dkennedychicago",
-		"location": "Chicago, Illinois"
+		"location": "Chicago, Illinois 60614"
 	},
 	
 		"welcomeMessage": "I am interested in working in Front-End Development in the Chicago Tech Industry.",
@@ -59,31 +59,40 @@ var education = {
 	"schools": [
 	{
 		"name": "University of California, UCLA",
-		"city": "Los Angeles",
+		"location": "405 Hilgard Avenue, Los Angeles 90095",
 		"degree": "Certificate",
 		"major": ["Entertainment Media (Cinematography)"],
 		"date": "2012 - 2012",
-		"url": "http://entertainment.uclaextension.edu/certificates/cinematography/"
+		"url": ["http://entertainment.uclaextension.edu/certificates/cinematography/"]
 			
 	},
 	{
 		"name": "Heriot-Watt University",
-		"city": "Edinburgh, Scotland",
+		"location": "Heriot-Watt University, Edinburgh Campus, Edinburgh, Scotland Edinburgh Scotland EH14 4AS",
 		"degree": "B.Sc",
 		"major": ["Architectural Engineering"],
 		"date": "1992",
-		"url": "http://www.hw.ac.uk/undergraduate/architectural-engineering.htm"
+		"url": ["http://www.hw.ac.uk/undergraduate/architectural-engineering.htm"]
+		
+	},
+	{
+		"name": "Limerick Institute Of Technology",
+		"location": "Limerick Institute Of Technology, Moylish Park, Limerick, Ireland.",
+		"degree": "Diploma",
+		"major": ["Architectural Engineering"],
+		"date": "1992",
+		"url": ["http://www.lit.ie/Courses/LC241/default.aspx"]
 		
 	}
 	],
 
 	"onlineCourses": [
 	{
-		"onlineClass": "Introduction to Programming",
+		"onlineClasses": "Introduction to Programming",
 		"onlineTitle": "Udacity",
 		"onlineSchool": "Nanodegree",
 		"onlineDates": "2015-2016",
-		"onlineURL": "https://www.udacity.com/course/intro-to-programming-nanodegree--nd000"
+		"onlineURL": ["https://www.udacity.com/course/intro-to-programming-nanodegree--nd000"]
 	}
 	]
 }
@@ -93,66 +102,80 @@ var work = {
 	{
 		"employer": "Gradient Effects",
 		"title": "VFX Editor | I\/O Coordinator",
-		"location": "Los Angeles",
+		"location": "4120 Del Rey Avenue, Marina Del Rey 90292",
 		"dates": "03\/12 - 06\/14",
 		"description": "Contributed to the delivery of high quality VFX for Film and Broadcast projects, on time and within budget. Instrumental in helping to develop in-house tools to automate the shot ingest and delivery pipeline. On smaller projects served as the key liaison between production, editorial and the show producer and supervisor. Managed a database of achievements and priorities, keeping the client informed and the crew on task."
 	},
 	{
 		"employer": "Modern Video Film",
 		"title": "Data I/O",
-		"location": "Los Angeles",
+		"location": "2300 W Empire Ave, Burbank CA 91504",
 		"dates": "03\/11 - 09\/11",
 		"description": "Worked in this very busy Data I/O department. Cloning, loading and unloading drives, and LTOs. File format conversions, backups and checksums. Scanning and recording of film assets, and Qc'ing DI reels for film out."
 	},
 	{
 		"employer": "Scanline VFX LA",
 		"title": "I\/O Coordinator | VFX Editor",
-		"location": "Los Angeles",
+		"location": "12910 Culver Blvd # C, Los Angeles 90066",
 		"dates": "10\/11 - 02\/12",
 		"description": "Worked on \"The Immortals\" VFX at Scanline LA as I/O Coordinator & VFX Editor. Using Avid MC and FCP FTP and Shotgun Database management."
 	},
 	{
 		"employer": "Prime Focus Hollywood",
 		"title": "Roto \/ Tasking Artist",
-		"location": "Hollywood CA",
+		"location": "1800 Vine St, Hollywood CA 90028",
 		"dates": "06\/10 - 08\/10",
 		"description": "Back at Prime this time as a tasking and Roto Artist for Cats and Dog Stereo Conversion, and Shrek BlueRay Stereo Conversion"
 	},
 	{
 		"employer": "Prologue Films",
 		"title": "Coordinator | Assoc.Producer",
-		"location": "Venice CA",
+		"location": "534 Victoria Ave, Venice CA 90291",
 		"dates": "10\//09 - 03\/10",
 		"description": "\“Titles Coordinator\” on \“Sherlock Holmes\” MT and MOE, Post Coordinator on HBO \"The Pacific\" Promo. Producer on HBO 24/7 Jimmy Johnson Broadcast GFX package. Associate Producer on 82nd Academy Awards Broadcast and Screen package. Coordinator on ESPN World Cup 2010 Broadcast Package."
 	},
 	{
 		"employer": "Prime Focus Hollywood",
 		"title": "Data I\/O | Render Wrangler",
-		"location": "Hollywood CA",
+		"location": "1800 Vine St, Hollywood CA 90028",
 		"dates": "07\/09 - 10\/09",
 		"description": "Prime Focus completed eight shots for the feature The Tree of Life, and over eighty for Avatar. My role as a Render Wrangler was in monitoring, prioritizing and troubleshooting (when needed) jobs spread over the render nodes of Prime's Los Angeles, Vancouver and Winnipeg facilities."
 	},      
 	{
 		"employer": "Pacific Title & Art Studio",
 		"title": "DI Color Assistant",
-		"location": "Hollywood CA",
+		"location": "2700 Santa Monica Blvd, Hollywood CA 90028",
 		"dates": "02\/07 - 07\/09",
 		"description": "As DI Assistant I supported Colorists, and Producers for Feature and Trailer Finishing. Note taking in DI sessions, and VFX screenings, updating cuts, monitoring renders, generating proxies, preparing film outs. QC, roto, dust busting, and secondary animation using Autodesk Lustre."
 	},
 	{
 		"employer": "Logan Media Inc",
 		"title": "Post Production Coordinator",
-		"location": "Hollywood CA",
+		"location": "4221 redwood ave, Venice CA 90066",
 		"dates": "07\/09 – 10\/09",
 		"description": "Supporting both producers and creatives on fast turnaround commercial GFX projects. Day to Day Asset management. DVD Authoring, Web compression, and FTP support."
 	},
 	{
 		"employer": "yU+Co",
 		"title": "Coordinator | Data I\/O",
-		"location": "Hollywood CA",
+		"location": "941 N. Mansfield Ave, Hollywood CA 90038",
 		"dates": "09\/03 – 09\/06",
 		"description": "Supporting producers and artist with asset management, ftp and website maintenance. Media management, scheduling, archiving, and delivery.On a range of projects Broadcast, GFX packages, live action commercials, VFX for TV and Film, and Film Main Titles."
-	}              
+	},
+	{
+		"employer": "Bay Area Video Coalition",
+		"title": "Facility Manager",
+		"location": "2727 Mariposa St, San Francisco, CA 94110",
+		"dates": "03\/01 – 05\/03",
+		"description": "Responsible for the coordination of facility technical staff and equipment for workshops. Technology upgrading, ordering, installation,duplication and Restoration."
+	},              
+	{
+		"employer": "Pacific Video Resources",
+		"title": "Coordinator | Data I\/O",
+		"location": "2331 Third St, San Francisco, 94107",
+		"dates": "06\/99 – 03\/01",
+		"description": "Facility Technician and Duplication Manager. Assistant Editing and graphics prep for online edits, graphics layoffs, troubleshooting, QC, maintenance and archiving assets on NLE. Avid’s and Discreet Smoke Systems."
+	}                                         
 	]
 }
 
@@ -254,41 +277,34 @@ projects.display();
 
 // EDUCATION
 
-var displayEducation = function() {
+education.display = function() {
   for (school in education.schools) {
     $("#education").append(HTMLschoolStart);
       var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
       var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
       var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].date);
-      var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].city);
+      var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
       var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
       var formattedEducation = formattedName + formattedDegree + formattedDates + formattedLocation + formattedMajor;
       
       $(".education-entry:last").append(formattedEducation);
-	}
-}
-displayEducation();
 
-var displayonlineCourses = function() {
+      }
 	for (courses in education.onlineCourses) {
-	    $("#onlineCourses").append(HTMLschoolStart);
-  		  var formattedonlineClasses = HTMLonlineClasses.replace("%data%", education.onlineCourses[courses].onlineClass);
-  		  var formattedonlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[courses].onlineTitle);
-  		  var formattedonlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[courses].onlineSchool);
-  		  var formattedonlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[courses].onlineDates);
-  		  var formattedonlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[courses].onlineURL);
-	  	  var formattedOnline = formattedonlineClasses + formattedonlineTitle + formattedonlineSchool + formattedonlineDates + formattedonlineURL;
-     	
-     	  $(".onlineCourses-entry:last").append(formattedOnline);
+	  $("#education").append(HTMLschoolStart);
+	    var formattedonlineClasses = HTMLonlineClasses.replace("%data%", education.onlineCourses[courses].onlineClasses);
+	    var formattedonlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[courses].onlineTitle);
+	    var formattedonlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[courses].onlineSchool);
+	    var formattedonlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[courses].onlineDates);
+	    var formattedonlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[courses].onlineURL);
+  	    var formattedonlineCourses = formattedonlineClasses + formattedonlineTitle + formattedonlineSchool + formattedonlineDates + formattedonlineURL;
+ 	
+ 	    $(".education-entry:last").append(formattedonlineCourses);
+
 	}
+	
 }
-displayonlineCourses();
-
-
-
-
-
-
+education.display();
 
 
 $(document).click(function(loc) {
@@ -312,5 +328,6 @@ $("#main").append(internationalizeButton);
 
 // MAP
 $("#mapDiv").append(googleMap);
+
 
 
